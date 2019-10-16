f9c1d51162df426824e8cd4955f5ae9db278c8e9
Alice D'Arcangelo <alicedarcangelo@Alices-MacBook-Pro.local>
2019-10-16 09:42:53 -0400

    changed background and images to reflect new project

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 $(() => {
-  //code here
+  console.log($(".menu"));
+  $(".menu_icon").on("click", event => {
+    event.preventDefault();
+    console.log($(event.currentTarget).siblings(0));
+    $(event.currentTarget)
+      .siblings(0)
+      .toggle();
+  });
+
+  const handleData = data => {
+    console.log(data);
+  };
 });
