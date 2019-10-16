799f9421fc4a2cb3cfaed0326ae1d76e302fa3a7
Alice D'Arcangelo <alicedarcangelo@Alices-MacBook-Pro.local>
2019-10-16 12:05:21 -0400

    changed api, got it up and running

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(() => {
   console.log($(".menu"));
   $(".menu_icon").on("click", event => {
     event.preventDefault();
-    console.log($(event.currentTarget).siblings(0));
+    // console.log($(event.currentTarget).siblings(0));
     $(event.currentTarget)
       .siblings(0)
       .toggle();
@@ -11,4 +11,9 @@ $(() => {
   const handleData = data => {
     console.log(data);
   };
+
+  $.ajax({
+    url:
+      "https://api.edamam.com/search?q=chicken&app_id=2c181b41&app_key=21818244f9316dd109a80ac7815443f7"
+  }).then(handleData);
 });
