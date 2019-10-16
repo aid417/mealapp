d08250dbf1b6cef8f4a9bb57d33006aa016d7691
Alice D'Arcangelo <alicedarcangelo@Alices-MBP.lan>
2019-10-16 17:28:36 -0400

    connected api with search page

--- js/search.js
+++ js/search.js
@@ -6,4 +6,23 @@ $(() => {
       .siblings(0)
       .toggle();
   });
+  const handleData = data => {
+    console.log(data);
+  };
+  $("form").on("submit", event => {
+    event.preventDefault();
+    const $q = $("#input-box").val();
+    console.log($q);
+    console.log(
+      `https://api.edamam.com/search?q=${$q}&app_id=2c181b41&app_key=21818244f9316dd109a80ac7815443f7`
+    );
+    $.ajax({
+      url: `https://api.edamam.com/search?q=${$q}&app_id=2c181b41&app_key=21818244f9316dd109a80ac7815443f7`
+    }).then(handleData);
+    $(event.currentTarget).trigger("reset");
+  });
+
+  // $.ajax({
+  //     url: `https://api.edamam.com/search?q=${$q}&app_id=2c181b41&app_key=21818244f9316dd109a80ac7815443f7`
+  //   }).then(handleData);
 });
