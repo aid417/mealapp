2ae9db775f89b08e2f3aea8ed68e1b3c86d80be5
Alice D'Arcangelo <alicedarcangelo@Alices-MacBook-Pro.local>
2019-10-17 16:58:55 -0400

    fixed api issue

--- js/search.js
+++ js/search.js
@@ -1,78 +1,47 @@
 $(() => {
- 
-  // let dataArray = [];
-  // const handleData = data => {
-  //   dataArray.push(data.hits);
-  //   let recipeArray = dataArray[0];
-  //   $(".recipes").attr("id", "white");
-  //   for (let i = 0; i < recipeArray.length; i++) {
-  //     console.log(recipeArray[i].recipe.label);
-  //     const $image = $("<img>");
-  //     $image.attr("src", recipeArray[i].recipe.image);
-  //     $image.attr("height", "50px");
-  //     $image.attr("width", "50px");
-  //     $image.addClass("resultimage");
-  //     const $a = $("<a>");
-  //     $a.attr("href", recipeArray[i].recipe.shareAs);
-  //     $a.text(recipeArray[i].recipe.source);
-  //     $a.attr("target", "_blank");
-  //     const $div = $("<div>");
-  //     $div.addClass("recipesearch");
+  let dataArray = [];
+  const handleData = data => {
+    dataArray.push(data.hits);
+    let recipeArray = dataArray[0];
+    $(".recipes").attr("id", "white");
+    for (let i = 0; i < recipeArray.length; i++) {
+      console.log(recipeArray[i].recipe.label);
+      const $image = $("<img>");
+      $image.attr("src", recipeArray[i].recipe.image);
+      $image.attr("height", "50px");
+      $image.attr("width", "50px");
+      $image.addClass("resultimage");
+      const $a = $("<a>");
+      $a.attr("href", recipeArray[i].recipe.shareAs);
+      $a.text(recipeArray[i].recipe.source);
+      $a.attr("target", "_blank");
+      const $div = $("<div>");
+      $div.addClass("recipesearch");
 
-  //     $div.text(recipeArray[i].recipe.label);
-  //     $div.append($image);
-  //     $div.append($a);
-  //     // console.log($div);
-  //     $(".recipes").append($div);
-  //   }
+      $div.text(recipeArray[i].recipe.label);
+      $div.append($image);
+      $div.append($a);
 
-  // $div.text(dataArray[i].recipe.label);
-  // $(".container").append($div);
-
-  // console.log(data.hits);
-  // console.log(dataArray);
-  // };
-  // export let months = [
-  //   "Jan",
-  //   "Feb",
-  //   "Mar",
-  //   "Apr",
-  //   "Aug",
-  //   "Sep",
-  //   "Oct",
-  //   "Nov",
-  //   "Dec"
-  // ];
-  export let name = "string";
-  export let apple = "golden";
-  export { name, apple };
-
+      $(".recipes").append($div);
+    }
+  };
+  // console.log(localStorage);
+  // localStorage.setItem("key", "value");
   $("form").on("submit", event => {
     event.preventDefault();
     const $q = $("#input-box").val();
-    $(event.currentTarget).trigger("reset");
-    let $mealType = "";
-    // if ($("#breakfast").prop() = ":checked") {
-    //   $mealType = $("#breakfast").val();
-    // } else if ($("#lunch").prop() = ":checked") {
-    //   $mealType = $("#lunch").val();
-    // } else if ($("#dinner").prop() = ":checked") {
-    //   $mealType = $("#dinner").val();
-    // }
-    console.log($mealType);
-    $(event.currentTarget).trigger("reset");
-    console.log($q);
+
+    // const $mealType = $("#meal-box").val();
+    // console.log($mealType);
+
+    // console.log($q);
     console.log(
-      `https://api.edamam.com/search?q=${$q}&app_id=2c181b41&app_key=21818244f9316dd109a80ac7815443f7&mealType=${$mealType}`
+      `https://api.edamam.com/search?q=eggs&app_id=2c181b41&app_key=21818244f9316dd109a80ac7815443f7`
     );
+    $(event.currentTarget).trigger("reset");
     $.ajax({
-      url: `https://api.edamam.com/search?q=${$q}&app_id=2c181b41&app_key=21818244f9316dd109a80ac7815443f7`
+      url: `https://api.edamam.com/search?q=${$q}&mealType=lunch&app_id=2c181b41&app_key=21818244f9316dd109a80ac7815443f7`
     }).then(handleData);
   });
-
-  // $.ajax({
-  //     url: `https://api.edamam.com/search?q=${$q}&app_id=2c181b41&app_key=21818244f9316dd109a80ac7815443f7`
-  //   }).then(handleData);
 });
-
-// {q: "eggs", from: 0, to: 10, params: {…}, more: true, …}count: 340146from: 0hits: (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]more: trueparams: {sane: Array(0), q: Array(1), app_key: Array(1), app_id: Array(1)}q: "eggs"to: 10__proto__: Object
+// url: `https://api.edamam.com/search?q=${$q}&app_id=2c181b41&app_key=21818244f9316dd109a80ac7815443f7&mealType=${$mealType}`
