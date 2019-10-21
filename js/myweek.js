c860ade6fa8011fbf0890336ba3b1c4343d1a654
Alice D'Arcangelo <alicedarcangelo@Alices-MacBook-Pro.local>
2019-10-21 09:49:00 -0400

    added about section to home page

--- js/myweek.js
+++ js/myweek.js
@@ -146,14 +146,17 @@ $(() => {
 
   $("form").on("submit", event => {
     event.preventDefault();
-    const $q = $("#input-box").val();
-
+    let $q = $("#input-box").val();
+    console.log($q);
     console.log(
-      `https://api.edamam.com/search?q=eggs&app_id=2c181b41&app_key=21818244f9316dd109a80ac7815443f7`
+      `https://api.edamam.com/search?q=${$q}&app_id=2c181b41&app_key=21818244f9316dd109a80ac7815443f7`
     );
-    $(event.currentTarget).trigger("reset");
+
     $.ajax({
       url: `https://api.edamam.com/search?q=${$q}&app_id=2c181b41&app_key=21818244f9316dd109a80ac7815443f7`
     }).then(handleData);
+    $(event.currentTarget).trigger("reset");
+    $("#input-box").val("");
+    console.log($("#input-box"));
   });
 });
